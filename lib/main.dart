4d8fd39a43d38a7373f66bc03318ad6a70bc2fdd
Garrett Luu <garrettluu131@gmail.com>
2021-04-26 05:12:38 -0700

    Changed Column to ListView

--- lib/main.dart
+++ lib/main.dart
@@ -75,22 +75,7 @@ class _MyHomePageState extends State<MyHomePage> {
       body: Center(
         // Center is a layout widget. It takes a single child and positions it
         // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
+        child: ListView(
           children: <Widget>[
             Text(
               "Hello, world!",
@@ -98,13 +83,6 @@ class _MyHomePageState extends State<MyHomePage> {
               style: TextStyle(fontSize: 24),
             ),
             SizedBox(height: 24),
-            // TaskWidget(
-            //     title: "Watch CSE 120 Lectures",
-            //     description: "Catch up on any missed lectures"),
-            // TaskWidget(
-            //   title: "Finish CSE 142L Lab",
-            //   description: "This will take a long time!",
-            // )
             ..._tasks
           ],
         ),
