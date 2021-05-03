c95b90866e10b63c40fceb4573baae1fedbf77b5
Garrett Luu <garrettluu131@gmail.com>
2021-05-02 23:13:20 -0700

    Clamped scrolling on home screen

--- lib/main.dart
+++ lib/main.dart
@@ -75,6 +75,7 @@ class _MyHomePageState extends State<MyHomePage> {
         // Center is a layout widget. It takes a single child and positions it
         // in the middle of the parent.
         child: ListView(
+          primary: false,
           children: <Widget>[
             SizedBox(height: 24),
             Text(
