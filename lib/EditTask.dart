f095536cc43e308a010369bb82abfb613e15d160
Garrett Luu <garrettluu131@gmail.com>
2021-05-02 17:53:39 -0700

    Added EditTask widget

---
+++ lib/EditTask.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class EditTask extends StatefulWidget {
+  EditTask({Key key}) : super(key: key);
+
+  @override
+  _EditTaskState createState() => _EditTaskState();
+}
+
+class _EditTaskState extends State<EditTask> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: null,
+    );
+  }
+}
